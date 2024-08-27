efa46ba3ab6535e857a2a6fa8cf95e42e2584cf6
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-27 22:50:40 +0530

    array  modified

--- 06-array.sh
+++ 06-array.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-FRUITS=("Apple" "Goa" "Banana")
-echo "Frist fruits is {$FRUITS[0]}"
-echo "Second  fruits is {$FRUITS[1]}"
+#FRUITS=("Apple" "Goa" "Banana")
+#echo "Frist fruits is {$FRUITS[0]}"
+#echo "Second  fruits is {$FRUITS[1]}"
+
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+echo "First fruit is : ${FRUITS[0]}"
+echo "Second fruit is : ${FRUITS[1]}"
+echo "Third fruit is : ${FRUITS[2]}"
+echo "All fruits are : ${FRUITS[@]}"
