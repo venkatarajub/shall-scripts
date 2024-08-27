aafc5e6deba7e64b614cc0582d0f9532d1acc42b
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-27 21:46:22 +0530

    Conversation script added

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How  are you?"
+echo "Suresh :: I am fine, how  do  you do?"
+echo "Ramesh :: I am  learning Shall script"
+echo "Suresh :: Nice to hear Ramesh"
