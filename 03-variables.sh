2a71cfb38db5f3e327931c5f37fd241e4aed15a7
Venkata <venkataraju.bandaru8@gmail.com>
2024-08-27 21:56:28 +0530

    Variable script added

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAJU
+PERSON2=VENKAT
+
+echo "$PERSON1 :: Hi $PERSON2, How  are you?"
+echo "$PERSON2 :: I am fine, how  do  you do?"
+echo "$PERSON1 :: I am  learning Shall script"
+echo "$PERSON2 :: Nice to hear $PERSON"
