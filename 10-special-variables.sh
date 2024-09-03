6028404d147621dd2e6b8a880e5bd16159f958e5
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-03 21:01:34 +0530

    Special variables

---
+++ 10-special-variables.sh
@@ -0,0 +1,5 @@
+echo "All variables passed to the script is $@"
+echo "How many variables passed to the script is $#"
+echo "Present script name  is $0"
+echo "Present working directory is $PWD"
+echo "Present home directory is $HOME" 
