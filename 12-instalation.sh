effb19059b5ddd671ea95816321516cc5da8b440
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-03 22:03:07 +0530

    instation script added

--- 12-instalation.sh
+++ 12-instalation.sh
@@ -9,4 +9,10 @@ then
     exit 1
 fi
 
-dnf install git -y
+dnf install mysql -y
+if [ $? -ne 0 ] #to check the given package installed  or not 0 mean installed
+then 
+    echo "mysql is  not istalled, installing mysql.."
+else
+    echo "mysql already installed. Nothing to  do.."
+fi
