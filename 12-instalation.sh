4f7460852dde2e3293c044492e0d99496e667b9a
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-03 21:55:52 +0530

    instation script added

--- 12-instalation.sh
+++ 12-instalation.sh
@@ -2,7 +2,7 @@
 #Install  mysql
 #id -u "To  get the user id, root user having 0"
 
-USERID=$id -u
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script with root privilages"
