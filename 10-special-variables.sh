0081e27a5a80ef178c55819fc872bf909b581015
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-03 21:06:05 +0530

    spcial variable added

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -2,4 +2,7 @@ echo "All variables passed to the script is $@"
 echo "How many variables passed to the script is $#"
 echo "Present script name  is $0"
 echo "Present working directory is $PWD"
-echo "Present home directory is $HOME" 
+echo "Present home directory is $HOME"
+echo "PID of current script $$"
+sleep 100 &
+echo  "PID of last backgroud command $!" 
