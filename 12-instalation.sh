7606ba1d39ce836836208b20a1490c4b6e55b3ec
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 07:06:34 +0530

    instalation script

--- 12-instalation.sh
+++ 12-instalation.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-#Install  mysql
-#id -u "To  get the user id, root user having 0"
-
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
+if [ $USERID -ne0 ]
 then
-    echo "Please run the script with root privilages"
-    exit 1
-fi
+    echo "Plrase run the script  with  root privilages"
 
-if [ $? -ne 0 ] #to check the given package installed  or not 0 mean installed
-then 
-    echo "mysql is  not istalled, installing mysql.."
-    dnf install mysql -y
-    if [ $? -ne 0 ]
-    then 
-        echo "mysql instalaton was not success ... check it"
-        exit 1
-    else
-        echo "mysql instalation success"
-    fi     
-else
-    echo "mysql already installed. Nothing to  do.."
 fi
+dnf install git -y
