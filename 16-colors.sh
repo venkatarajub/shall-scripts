278f03fdd187cbdb9a076fe629b44893da1326c2
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 22:22:03 +0530

    color script

---
+++ 16-colors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[36m"
+G="\e[35m"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e  "$2 is ..$R FAILED $N"
+        exit 1
+    else 
+        echo "$2 is .. $G SUCCESS $N"
+    fi
+}
+if [ $USERID -ne 0 ]
+then
+    echo "Run the script with root previlages"
+    exit 1
+fi
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo "nginx not installed. going to install..."
+    dnf install nginx -y
+    VALIDATE $? "Installing nginx"  
+else 
+    echo "nginx already installed nothing to do.."
+fi
