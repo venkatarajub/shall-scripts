310a9383f55227a48306bacca6fee00b0c4d616e
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 21:37:05 +0530

    fun-install

--- 15-functions-instalation.sh
+++ 15-functions-instalation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-USERID=(id -u)
+USERID=$(id -u)
 VALIDATE(){
     echo  "exit status : $1"
 }
 
 if [ $USERID -ne 0 ]
 then 
-    echo "Run the script with root privillages"
+    echo "Run the script with root privilages"
     exit 1
 fi
 dnf list installed nginx
