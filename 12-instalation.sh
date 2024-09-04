85faf07218077e08f8f2a9abac1685e97aec182f
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 10:14:37 +0530

    instalation script

--- 12-instalation.sh
+++ 12-instalation.sh
@@ -11,6 +11,13 @@ if [ $? -ne 0 ]
 then
     echo "git is  not installed going to install"
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git instalation not success. pleae check"
+        exit 1
+    else 
+        echo "git instalation sucess"
+    fi     
 else
     echo "git already installed. nothing to do.."
 fi
