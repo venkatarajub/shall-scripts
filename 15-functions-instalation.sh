b716f326aef1ab86be078e9af10537744bd5282a
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 21:58:13 +0530

    fun-install

--- 15-functions-instalation.sh
+++ 15-functions-instalation.sh
@@ -2,18 +2,31 @@
 USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then
-        echo "$2 is ...FAILED"
+    then 
+        echo  "$2 is .. FAILED"
         exit 1
     else 
-        echo "$2 is ...SUCESS"
+        echo "$2 is .. SUCCESS"
     fi
 }
-
 if [ $USERID -ne 0 ]
-then 
-    echo "Run the script with root privilages"
+then
+    echo "Run the script with root previlages"
     exit 1
 fi
-dnf list installed git
-VALIDATE $? "listing git"
+dnf list installed nginx
+VALIDATE $? "Listing nginx"
+# if [ $? -ne 0 ]
+# then
+#     echo "nginx not installed. going to install..."
+#     dnf install nginx -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "nginx instalation failed. Please check"
+#         exit 1
+#     else
+#         echo  "nginx instalsation successfully completed."
+#     fi  
+# else 
+#     echo "nginx already installed nothing to do.."
+# fi
