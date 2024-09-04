7ebd1803b47d9d6d0443ff81741665cb732639f8
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 07:15:52 +0530

    instalation script

--- 12-instalation.sh
+++ 12-instalation.sh
@@ -6,4 +6,11 @@ then
     exit 1
 
 fi
-dnf install git -y
+dnf  list installed git
+if [ $? -ne 0 ]
+then
+    echo "git is  not installed going to install"
+    dnf install git -y
+else
+    echo "git already installed. nothing to do.."
+fi
