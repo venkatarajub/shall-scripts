7ba37385ff25d20a725cc9259d85b331a66be679
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-04 18:42:49 +0530

    nginx instalation script

--- 13-instalation-nginx.sh
+++ 13-instalation-nginx.sh
@@ -5,4 +5,16 @@ then
     echo "Run the script with root previlages"
     exit 1
 fi
-dnf install nginx -y
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx not installed. going to install..."
+    if [ $? -ne 0 ]
+    then
+        echo "nginx instalation failed. Please check"
+        exit 1
+    elase
+        echo  "nginx instalsation successfully completed."
+    fi  
+elase 
+    echo "nginx already installed nothing to do.."
