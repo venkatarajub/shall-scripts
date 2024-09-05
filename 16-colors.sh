6c0a5a6ee65b22a6bea84f90c036c200a9ecfbdf
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-05 10:02:07 +0530

    loops scrit added

--- 16-colors.sh
+++ 16-colors.sh
@@ -3,6 +3,13 @@ USERID=$(id -u)
 R="\e[36m"
 G="\e[35m"
 Y="\e[33"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+then
+    echo "Run the script with root previlages"
+    exit 1
+fi 
+}
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
@@ -12,18 +19,9 @@ VALIDATE(){
         echo -e "$2 is .. $G SUCCESS $N"
     fi
 }
-if [ $USERID -ne 0 ]
-then
-    echo "Run the script with root previlages"
-    exit 1
-fi
-dnf list installed nginx
 
-if [ $? -ne 0 ]
-then
-    echo "nginx not installed. going to install..."
-    dnf install nginx -y
-    VALIDATE $? "Installing nginx"  
-else 
-    echo -e $Y "nginx already installed nothing to do.." $N
-fi
+CHECK_ROOT
+for package in $@
+do
+    echo $package
+done
---
+++ 18-loops.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Print 1 to 50 number
+#for this we are using  for loop
+for i in {0..50}
+do 
+    echo "$i"
+done
