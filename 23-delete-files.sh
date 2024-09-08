47481afd6a6721a2f431a50470400613e332e0c9
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-08 16:49:22 +0530

    delete script added

--- 23-delete-files.sh
+++ 23-delete-files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SOURCE_DIR="/home/ec2-user"
 
-if [ -d $SOURCE_DIR -eq 0 ]
+if [ -d $SOURCE_DIR -ne 0 ]
 then
-    echo "directory exists"
-else
-    echo "directory  not exist"
+    echo "directory not exists"
     exit 1
+else
+    echo "directory exists"
 fi
 
 
 find /home/ec2-user -name "*.log" -mtime +14
- 
+
 
 
