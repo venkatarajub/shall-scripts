1e47df132a2d3bdfc22ab1d6cc1e1c34d8f41d3e
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-08 23:07:43 +0530

    diskusage script added

---
+++ 26-diskusage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT)
+DISK_THRESHOLD=5
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [$USAGE -gt  $DISK_THRESHOLD ]
+    then
+        echo $PARTITION is more than $DISK_THRESHOLD and curret value is $USAGE
+    fi
+done <<< $DISK_USAGE
